76d5a419fddbdf941708b988a4fd9057a64058e4
matheus-gomesf <matheusg.code@gmail.com>
2023-12-20 17:17:34 -0300

    Creating product mapper.

---
+++ product-service/src/main/java/com/br/productservice/mapper/ProductMapper.java
@@ -0,0 +1,16 @@
+package com.br.productservice.mapper;
+
+import com.br.productservice.dto.ProductDTO;
+import com.br.productservice.entity.ProductEntity;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+@Mapper(componentModel = "spring")
+public interface ProductMapper {
+
+    ProductMapper PRODUCT_MAPPER = Mappers.getMapper(ProductMapper.class);
+
+    ProductEntity dtoToEntity(ProductDTO record);
+
+    ProductDTO entityToDto(ProductEntity entity);
+}
