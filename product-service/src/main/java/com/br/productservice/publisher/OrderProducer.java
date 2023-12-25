3742ba96db2019cf0119d85dd5e6c283c05f3db8
matheus-gomesf <matheusg.code@gmail.com>
2023-12-25 20:54:25 -0300

    Rabbitmq configuration.

---
+++ product-service/src/main/java/com/br/productservice/publisher/OrderProducer.java
@@ -0,0 +1,23 @@
+package com.br.productservice.publisher;
+
+import com.br.productservice.dto.OrderDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class OrderProducer {
+
+    @Value("${rabbitmq.exchange.name}")
+    private String exchangeName;
+    @Value("${rabbitmq.routing.key}")
+    private String routingKey;
+
+    private final RabbitTemplate rabbitTemplate;
+
+    public void orderCreated(OrderDTO orderDTO) {
+        rabbitTemplate.convertAndSend(exchangeName, routingKey, orderDTO);
+    }
+}
