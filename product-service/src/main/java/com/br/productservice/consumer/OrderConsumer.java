5b3c945696a4565893ee84be0eee1adeb7c3c51f
matheus-gomesf <matheusg.code@gmail.com>
2023-12-25 21:19:21 -0300

    Config order consumer.

---
+++ product-service/src/main/java/com/br/productservice/consumer/OrderConsumer.java
@@ -0,0 +1,18 @@
+package com.br.productservice.consumer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class OrderConsumer {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(OrderConsumer.class);
+
+    @RabbitListener(queues = "${rabbitmq.queue.name}")
+    private void orderConsumer(String message) {
+        LOGGER.info(message);
+    }
+
+}
